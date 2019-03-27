88454e07f85bbf0810eeb555df315c3ff8768a53
Kaitlyn Warboy <48352960+kmwarboy@users.noreply.github.com>
2019-03-27 17:38:27 -0400

    Updated sass files, added js files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// console.log("hello world");
+// this is a comment - type, then press command /
+// var x = "hello world";
+// alert(x);
+document.addEventListener('click', function(){
+    // block = visible
+    document.getElementsByClassName('search-bar').style.display='block';
+})
