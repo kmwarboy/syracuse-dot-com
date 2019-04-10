373ef7ea2164f5688d0ac9c49cad53d087c3f82e
Kaitlyn Warboy <48352960+kmwarboy@users.noreply.github.com>
2019-04-09 20:01:38 -0400

    Updated CSS and content for secondary & tertiary

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-// console.log("hello world");
-// this is a comment - type, then press command /
-// var x = "hello world";
-// alert(x);
-document.addEventListener('click', function(){
-    // block = visible
-    document.getElementsByClassName('search-bar').style.display='block';
-})
+console.log("hello world");
